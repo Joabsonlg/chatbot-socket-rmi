90767b59d191d2bbcac8655bad477aa42a00d811
Caio Vitor <caiovitordm@protonmail.com>
2024-03-18 12:10:52 -0300

    feat: integração da biblioteca do chatbot em socket

--- src/main/java/ufrn/br/dim0614/SocketServerImpl.java
+++ src/main/java/ufrn/br/dim0614/SocketServerImpl.java
@@ -1,17 +1,29 @@
 package ufrn.br.dim0614;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.PrintWriter;
+import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.Set;
 import java.util.concurrent.ConcurrentHashMap;
+import org.alicebot.ab.Bot;
+import org.alicebot.ab.Chat;
+import org.alicebot.ab.MagicBooleans;
 
 public class SocketServerImpl implements ChatBotServerInterface {
     private static final int PORT = 4444;
     private static final Set<PrintWriter> CLIENT_WRITERS = ConcurrentHashMap.newKeySet();
+    private static final boolean TRACE_MODE = false;
+    private static Bot bot;
+    private static Chat chatSession;
+
+    public SocketServerImpl() {
+        String resourcesPath = getResourcesPath();
+        bot = new Bot("super", resourcesPath);
+        MagicBooleans.trace_mode = TRACE_MODE;
+        chatSession = new Chat(bot);
+        bot.brain.nodeStats();;
+        bot.brain.nodeStats();
+    }
 
     @Override
     public void startServer() {
@@ -19,7 +31,7 @@ public class SocketServerImpl implements ChatBotServerInterface {
             System.out.println("ChatBot Server (Socket) is listening on port " + PORT);
 
             while (true) {
-                new ClientHandler(serverSocket.accept()).start();
+                new ClientHandler(serverSocket.accept(), chatSession).start();
             }
         } catch (IOException e) {
             System.out.println("Exception in ChatBot Server (Socket): " + e.getMessage());
@@ -29,9 +41,11 @@ public class SocketServerImpl implements ChatBotServerInterface {
 
     private static class ClientHandler extends Thread {
         private final Socket socket;
+        private final Chat chatSession;
 
-        public ClientHandler(Socket socket) {
+        public ClientHandler(Socket socket, Chat chatSession) {
             this.socket = socket;
+            this.chatSession = chatSession;
         }
 
         @Override
@@ -44,6 +58,8 @@ public class SocketServerImpl implements ChatBotServerInterface {
                 String message;
                 while ((message = input.readLine()) != null) {
                     System.out.println("Received: " + message);
+                    String response = chatSession.multisentenceRespond(message);
+                    output.println(response);
                     for (PrintWriter writer : CLIENT_WRITERS) {
                         if (writer != output) {
                             writer.println("From another client: " + message);
@@ -62,4 +78,13 @@ public class SocketServerImpl implements ChatBotServerInterface {
             }
         }
     }
+
+    private static String getResourcesPath() {
+        File currDir = new File(".");
+        String path = currDir.getAbsolutePath();
+        path = path.substring(0, path.length() - 1);
+        String resourcesPath = path + "src" + File.separator + "main" + File.separator + "resources";
+        return resourcesPath;
+    }
+
 }
